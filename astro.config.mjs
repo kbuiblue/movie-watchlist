6aa4cb0d9885b296ec9f04f7e0f8a9051b1acdb7
Khanh Bui <kbuiblue@gmail.com>
2023-08-22 01:34:00 +0700

    Add Svelte and re-add nanostore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify/functions";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [svelte()]
 });
