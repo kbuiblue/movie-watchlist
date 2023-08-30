d96bd09101770bfa54f15059123f24b378f6b94d
Khanh Bui <kbuiblue@gmail.com>
2023-08-31 02:13:53 +0700

    Update to Astro 3.0 and remove SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-import netlify from "@astrojs/netlify/functions";
 
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: netlify(),
   integrations: [svelte()]
 });
